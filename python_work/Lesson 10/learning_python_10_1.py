b594ca5711b43bfc69a5f6a4fd3254861221cdd6
Alex Flores <alexontheiphone@gmail.com>
2025-03-01 00:34:22 -0800

    Update 2/28/25

--- python_work/Lesson 10/learning_python_10_1.py
+++ python_work/Lesson 10/learning_python_10_1.py
@@ -1,9 +1,9 @@
 # practice 2/28/25
 # chapter 10
 # learning python
-
+import os
 from pathlib import Path as p # import lib
-
-path = p('learning_python.txt') # label the path to the file
+print(os.getcwd())
+path = p('python_work/Lesson 10/learning_python.txt') # Specify the relative path
 contents = path.read_text() # reading whole file
 print(contents)
