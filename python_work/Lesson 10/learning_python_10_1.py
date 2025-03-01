0251b0a0021abde2b348465ba146e897216cf991
Alex Flores <alexontheiphone@gmail.com>
2025-03-01 01:04:40 -0800

    Update Lesson 10

--- python_work/Lesson 10/learning_python_10_1.py
+++ python_work/Lesson 10/learning_python_10_1.py
@@ -6,9 +6,9 @@ from pathlib import Path as p # import lib
 print(os.getcwd())
 path = p('python_work/Lesson 10/learning_python.txt') # Specify the relative path
 contents= path.read_text() # reading whole file
-print(contents)
-print('\n')
+print(contents) # Prints the whole file as is.
+print('\n') # Break.
 
-lines = contents.splitlines()
-for line in lines:
-    print(line)
+lines = contents.splitlines() # Reads the file and return list of each line of the file.
+for line in lines: # Reiterate through the list.
+    print(line) # Print out each line of the file
